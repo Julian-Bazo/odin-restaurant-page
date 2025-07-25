c3296e7d59783ccbbdc6da7f82a1d6747a8830ea
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-07-25 01:22:03 -0400

    Create base homepage and styling + img upload
    Creates the base homepage and styling. The home module is added as the default webpage.

---
+++ src/home.js
@@ -0,0 +1,26 @@
+import mainImg from "./restaurant.jpg";
+
+export default function createWebpage() {
+    const contentContainer = document.querySelector("#content");
+
+
+    const header = document.createElement("header");
+    header.textContent = "Jbaz's Fine Dining"
+    header.style.fontSize = "4rem";
+    header.style.marginTop = "1rem";
+    contentContainer.appendChild(header);
+
+    const titleImg = document.createElement("img");
+    titleImg.src = mainImg;
+    titleImg.alt = "A photo of our fine restaurant";
+    titleImg.style.height = "20rem";
+    contentContainer.appendChild(titleImg);
+
+    const titleText= document.createElement("p");
+    titleText.textContent = "Here is a photo of our great restaurant. We take pride in serving home-cooked meals with the freshest ingredients sourced from local farms. Come dine with us, you won't regret it!";
+    titleText.style.fontSize = "1.5rem";
+    titleText.style.width = "45ch";
+    titleText.style.marginLeft = ".5rem"
+    contentContainer.appendChild(titleText);
+
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./styles.css";
 console.log("hello World");
+import createContent from "./home";
+
+createContent();
